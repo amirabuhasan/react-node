ae519a632d1c6687fb247cce85843c5388e94ed5
amirabuhasan <amirabuhasan@gmail.com>
2018-06-14 23:53:56 +0800

    Initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+
+module.exports = (app) => {
+  // route handler
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  // route handler to take code from url
+  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
